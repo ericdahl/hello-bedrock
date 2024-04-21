c1454a8730f6a868556c88c8becfff81eaf5a2dd
Eric Dahl <eric.c.dahl@gmail.com>
2024-04-21 11:41:23 -0700

    add function

--- main.py
+++ main.py
@@ -11,29 +11,25 @@ brt = boto3.client(service_name='bedrock-runtime')
 # })
 
 
-body = json.dumps({
-    "prompt": '\n\nHuman: write an essay for living on mars in 100 words\n\nAssistant:',
-    "max_gen_len": 512,
-    "temperature": 0.1,
-    "top_p": 0.9
-})
+def prompt_llama(prompt: str):
+    body = json.dumps({
+        "prompt": prompt,
+        "max_gen_len": 512,
+        "temperature": 0.1,
+        "top_p": 0.9
+    })
+    response = brt.invoke_model_with_response_stream(
+        modelId='meta.llama2-13b-chat-v1',
+        body=body
+    )
 
-    #modelId='anthropic.claude-v2', 
-response = brt.invoke_model_with_response_stream(
-    modelId='meta.llama2-13b-chat-v1',
-    body=body
-)
+    stream = response.get('body')
+    if stream:
+        for event in stream:
+            chunk = event.get('chunk')
+            if chunk:
+                d = json.loads(chunk.get('bytes').decode())
+                print(d['generation'], end='')
+                sys.stdout.flush()
 
-stream = response.get('body')
-if stream:
-    for event in stream:
-        chunk = event.get('chunk')
-        if chunk:
-            d = json.loads(chunk.get('bytes').decode())
-            print(d['generation'], end='')
-            sys.stdout.flush()
-#
-# response = brt.invoke_model(modelId='meta.llama2-13b-chat-v1', body=body)
-# print(response)
-# response_body = json.loads(response.get('body').read())
-# print(response_body)
+prompt_llama("Human: write a python function to compute the min, mean and max of a list of integers. Assistant: ")
