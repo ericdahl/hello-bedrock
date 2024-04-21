ae551814999820052fdee7ed19d8a9746e25d6db
Eric Dahl <eric.c.dahl@gmail.com>
2024-04-21 12:05:23 -0700

    add titan

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ import sys
 
 brt = boto3.client(service_name='bedrock-runtime')
 
-# body = json.dumps({
-#     'prompt': '\n\nHuman: write an essay for living on mars in 1000 words\n\nAssistant:',
-#     'max_tokens_to_sample': 4000
-# })
+def prompt_titan_text(prompt: str):
 
+    body = json.dumps({
+        "inputText": prompt,
+        "textGenerationConfig": {
+            "maxTokenCount": 4096,
+            "stopSequences": [],
+            "temperature": 0,
+            "topP": 1
+        }
+    })
+
+    response = brt.invoke_model_with_response_stream(
+        modelId='amazon.titan-text-lite-v1',
+        body=body
+    )
+    print(response)
+    for e in response['body']:
+        out_tokens = json.loads(e['chunk']['bytes'])['outputText']
+        print(out_tokens)
 
 def prompt_llama(prompt: str):
     body = json.dumps({
@@ -32,4 +47,6 @@ def prompt_llama(prompt: str):
                 print(d['generation'], end='')
                 sys.stdout.flush()
 
-prompt_llama("Human: write a python function to compute the min, mean and max of a list of integers. Assistant: ")
+prompt = "Human: write a python function to compute the min, mean and max of a list of integers. Assistant: "
+# prompt_llama(prompt)
+prompt_titan_text(prompt)
